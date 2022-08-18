879ad2d5d0a492786b179969386179bb75ef942e
Jayadi Akbar <jayadiakbar22@gmail.com>
2022-08-18 11:21:43 +0700

    commit smooth scroll

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { faCartArrowDown, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-navbar',
@@ -18,8 +17,26 @@ export class NavbarComponent implements OnInit {
 
   constructor(private router: Router){}
 
-  public buttonClick(fragment: string): void {
-    this.router.navigateByUrl('#' + fragment);
+
+
+
+  toDemos(){
+    document.getElementById("demos")?.scrollIntoView({behavior:"smooth"});
+  }
+  toAbout(){
+    document.getElementById("about")?.scrollIntoView({behavior:"smooth"});
+  }
+  toFeatures(){
+    document.getElementById("features")?.scrollIntoView({behavior:"smooth"});
+  }
+  toPortfolio(){
+    document.getElementById("portfolio")?.scrollIntoView({behavior:"smooth"});
+  }
+  toShop(){
+    document.getElementById("shop")?.scrollIntoView({behavior:"smooth"});
+  }
+  toContact(){
+    document.getElementById("contact")?.scrollIntoView({behavior:"smooth"});
   }
 
 
